caa84d7c5d2f0c84feb1465d798bf1d728c481d3
at-luantran <luan.tran@asiantech.vn>
2018-08-07 15:11:15 +0700

    Model User

---
+++ db/migrate/20180807075401_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_digest
+      t.string :remember_digest
+      t.string :role
+      t.string :activation_digest
+      t.datetime :activated_at
+      t.string :reset_digest
+      t.datetime :reset_sent_at
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
