ccc2c3876a2057bab384fd2be775a88a51397ed0
at-tanvo <tan.vo@asiantech.vn>
2018-08-08 08:53:42 +0700

    Activation account

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+
+  before_action :find_user, only: %i[show edit update correct_user destroy]
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user, only: %i[edit update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Update success'
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = 'User deleted'
+    redirect_to users_url
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_comfirm)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+    redirect_to '/404' unless @user
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :account_activations, only: [:edit]
 end
