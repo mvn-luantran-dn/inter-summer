371051f128705efd6844674929d8f19e112045e6
at-luantran <luan.tran@asiantech.vn>
2018-08-10 15:17:47 +0700

    Fix assciation in model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :item
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :detail, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
+  validates :price_at, presence: true
 end
---
+++ db/migrate/20180809043038_create_auction_details.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateAuctionDetails < ActiveRecord::Migration[5.2]
+  def change
+    create_table :auction_details do |t|
+      t.references :auction, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string :price_bid
+
+      t.timestamps
+    end
+  end
+end
