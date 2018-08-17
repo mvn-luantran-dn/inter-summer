923de16bb0de35f158c1dd0875472424d4c8190d
at-tanvo <tan.vo@asiantech.vn>
2018-08-17 11:00:00 +0700

    checkout page and cart count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_102244) do
+ActiveRecord::Schema.define(version: 2018_08_17_021654) do
 
   create_table "assets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "file"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2018_08_16_102244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "name"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
