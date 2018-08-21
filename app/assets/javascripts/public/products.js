0e93be0946030f3219740d730c0f5bdb2afc1a9d
at-tanvo <tan.vo@asiantech.vn>
2018-08-21 11:12:50 +0700

    Slide Image

--- app/assets/javascripts/public/products.js
+++ app/assets/javascripts/public/products.js
@@ -1,32 +1,36 @@
-//= require jquery.elevatezoom
 //= require jquery
 
-jQuery(function($) {
+// jQuery(function($) {
 
-  // define the gallery object
-  var $gallery = $('#js-gallery_id_60297');
+//   // define the gallery object
+//   var $gallery = $('#js-gallery');
 
-  // Build array of objects to open in Fancybox.
-  var $imgs = [];
-  $('a', $gallery).each(function() {
-    $imgs.push({'src': $(this).data('zoom-image')});
-  });
+//   // Build array of objects to open in Fancybox.
+//   var $imgs = [];
+//   $('a', $gallery).each(function() {
+//     $imgs.push({'src': $(this).data('zoom-image')});
+//   });
 
-  // Initiate ElevateZoom
-  $("#js-ContentPlaceHolder1_imgProductPhoto").elevateZoom({
-    gallery: $gallery.attr('id'),
-    cursor: 'pointer',
-    galleryActiveClass: 'active',
-    imageCrossfade: true,
-    loadingIcon: 'loading.gif'
-  });
+//   // Initiate ElevateZoom
+//   $("#js-ContentPlaceHolder1_imgProductPhoto").elevateZoom({
+//     gallery: $gallery.attr('id'),
+//     cursor: 'pointer',
+//     galleryActiveClass: 'active',
+//     imageCrossfade: true,
+//     loadingIcon: 'loading.gif'
+//   });
 
-  // Bind Fancybox to clicking the zoom image.
-  // Open it to the currently active index.
-  $("#js-ContentPlaceHolder1_imgProductPhoto").on("click", function(e) {
-    e.preventDefault();
-    var active_index = $('a.active', $gallery).index();
-    $.fancybox.open($imgs, false, active_index);
-  });
+//   // Bind Fancybox to clicking the zoom image.
+//   // Open it to the currently active index.
+//   $("#js-ContentPlaceHolder1_imgProductPhoto").on("click", function(e) {
+//     e.preventDefault();
+//     var active_index = $('a.active', $gallery).index();
+//     $.fancybox.open($imgs, false, active_index);
+//   });
 
-});
+// });
+
+function changeImg(img) {
+  var expandedImg = document.getElementById('expandImg');
+  expandedImg.src = img.src;
+}
