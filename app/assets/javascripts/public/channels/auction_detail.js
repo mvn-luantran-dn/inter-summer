4db22895fabb63cf5fbb9d16d5d9013255f57ddf
at-luantran <luan.tran@asiantech.vn>
2018-08-21 10:10:42 +0700

    Fix manage timer

--- app/assets/javascripts/public/channels/auction_detail.js
+++ app/assets/javascripts/public/channels/auction_detail.js
@@ -7,13 +7,21 @@ $(document).on('turbolinks:load', function () {
         timer_id: check.dataset.timerId,
       },
       {
-        connected: function(data){
+        conntected: function () {
+          swal.close()
+        },
+        disconnected: function () {
+          swal(
+            'Lost connection',
+            'Please check again!',
+            'warning'
+          )
         },
         received: function(data) {
           data = data.obj;
           $('#spanCountDown').html(time_convert(data.period));
           $('#ContentPlaceHolder1_lbTitle').html(data.product_name);
-          $('#product-price').html(formatMoney(data.product_price));
+          $('#product-price').html(formatMoney(data.product_price_start));
           $('#detail-product').html(data.product_detail);
           $('#first-image').html('<img src="' + data.product_pictures[0].file_name.url + '" alt="' + data.product_name + ' class="slow-images" style="max-width: 100%;border-bottom: 1px solid #ccc;border-right: 1px solid #ccc;"">');
           html = "";
@@ -23,5 +31,9 @@ $(document).on('turbolinks:load', function () {
           $('#js-gallery').html(html);
         }
     });
+  } else {
+      if(App.auction_detail != undefined) {
+      App.auction_detail.unsubscribe()
+    }
   }
 });
--- lib/datas/auction_data.rb
+++ lib/datas/auction_data.rb
@@ -17,6 +17,7 @@ class AuctionData
         product_id: obj.product_id,
         product_name: obj.product.name,
         product_price: obj.product.price,
+        product_price_start: obj.product.price_at,
         product_quantity: obj.product.quantity,
         product_pictures: obj.product.assets,
         product_detail: obj.product.detail,
@@ -50,7 +51,7 @@ class AuctionData
 
   def reset_auction_price(auction)
     auction['period'] = load_period_default(auction['id'])
-    auction['product_price'] = load_price_default(auction['id'])
+    auction['product_price_start'] = load_price_default(auction['id'])
     $redis.set(auction['id'], auction.to_json)
   end
 
@@ -61,6 +62,50 @@ class AuctionData
 
   def load_price_default(id)
     auction = Timer.find_by(id: id)
-    auction.product.price
+    auction.product.price_at
+  end
+
+  def self.add(obj)
+    time = format_time_to_seconds(obj.period)
+    data = {
+      id: obj.id,
+      start_at: obj.start_at,
+      end_at: obj.end_at,
+      period: time,
+      bid_step: obj.bid_step,
+      status: obj.status,
+      product_id: obj.product_id,
+      product_name: obj.product.name,
+      product_price: obj.product.price,
+      product_price_start: obj.product.price_at,
+      product_quantity: obj.product.quantity,
+      product_pictures: obj.product.assets,
+      product_detail: obj.product.detail,
+      product_category: obj.product.category_id
+    }
+    $redis.set(obj.id, data.to_json)
+  end
+  
+  def update(timer)
+    data = JSON.parse($redis.get(timer.id))
+    period = format_time_to_seconds(timer.period)
+    data['start_at'] = timer.start_at
+    data['end_at'] = timer.end_at
+    data['bid_step'] = timer.bid_step
+    data['product_id'] = timer.product_id
+    data['product_name'] = timer.product.name
+    data['product_price'] = timer.product.price
+    data['product_detail'] = timer.product.detail
+    data['product_price_start'] = timer.product.price_at
+    data['product_quantity'] = timer.product.quantity
+    data['product_pictures'] = timer.product.assets
+    data['product_category'] = timer.product.category_id
+    data['period'] = period
+    data['status'] = timer.status
+    $redis.set(timer.id, data.to_json)
+  end
+  
+  def self.delete(obj)
+    $redis.del(obj.id)
   end
 end
