b5bf9f6e65d0bdf1965b4affd38c49110701c143
at-tanvo <tan.vo@asiantech.vn>
2018-08-21 17:21:08 +0700

    Hide Show Bidding History

--- app/assets/javascripts/public/products.js
+++ app/assets/javascripts/public/products.js
@@ -4,3 +4,8 @@ function changeImg(img) {
   var expandedImg = document.getElementById('expandImg');
   expandedImg.src = img.src;
 }
+$(document).ready(function(){
+  $("#show_history").click(function(){
+      $("#view_history").fadeToggle("slow");
+  });
+});
