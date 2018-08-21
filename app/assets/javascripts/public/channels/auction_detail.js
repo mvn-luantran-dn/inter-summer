258bcdae55a12fd5b7dcdb93257e9e629f4505e7
at-tanvo <tan.vo@asiantech.vn>
2018-08-21 11:12:50 +0700

    Slide Image

--- app/assets/javascripts/public/channels/auction_detail.js
+++ app/assets/javascripts/public/channels/auction_detail.js
@@ -15,12 +15,16 @@ $(document).ready(function () {
           $('#ContentPlaceHolder1_lbTitle').html(data.product_name);
           $('#product-price').html(formatMoney(data.product_price));
           $('#detail-product').html(data.product_detail);
-          $('#first-image').html('<img src="' + data.product_pictures[0].file_name.url + '" alt="' + data.product_name + ' class="slow-images" style="max-width: 100%;border-bottom: 1px solid #ccc;border-right: 1px solid #ccc;"">');
-          html = "";
-          data.product_pictures.forEach(function (e) {
-             html += '<li class="product_thumb"><img class="slow-images" src="' + e.file_name.url + '"></li>';
-          });
-          $('#js-gallery').html(html);
+          // $('#first-image').html('<img id="js-ContentPlaceHolder1_imgProductPhoto" class="slow-images" data-zoom-image="' + data.product_pictures[0].file_name.url + '" src="' + data.product_pictures[0].file_name.url +'" style="max-width: 100%;border-bottom: 1px solid #ccc;border-right: 1px solid #ccc;">')
+          // $('#js-gallery').html('<li><a href="#" id="ContentPlaceHolder1_aProduct1" class="product_thumb img72 active" data-image="' + data.product_pictures[0].file_name.url + '" data-zoom-image="' + data.product_pictures[0].file_name.url + '"><img id="ContentPlaceHolder1_imgGallery1" class="slow-images" src="' + data.product_pictures[0].file_name.url + '"></a></li><li><a href="#" id="ContentPlaceHolder1_aProduct2" class="product_thumb img72" data-image="' + data.product_pictures[1].file_name.url + '" data-zoom-image="' + data.product_pictures[1].file_name.url + '"><img id="ContentPlaceHolder1_imgGallery2" class="slow-images" src="' + data.product_pictures[1].file_name.url + '"></a></li><li><a href="#" id="ContentPlaceHolder1_aProduct3" class="product_thumb img72" data-image="' + data.product_pictures[2].file_name.url + '" data-zoom-image="' + data.product_pictures[2].file_name.url + '"><img id="ContentPlaceHolder1_imgGallery3" class="slow-images" src="' + data.product_pictures[2].file_name.url + '"></a></li><li></li><li></li>')
+
+
+          // html = "";
+          // data.product_pictures.forEach(function (e) {
+          //    html += '<div class="item active"><img src="' + e.file_name.url +'" alt="" style="width:100%;"></div>';
+          // });
+          // $('#first-image').html(html);
+ 
         }
     });
   }
