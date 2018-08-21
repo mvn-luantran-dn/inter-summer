162e20744f460416e49baf06bfa2e959d72e4c6c
at-luantran <luan.tran@asiantech.vn>
2018-08-21 11:15:12 +0700

    Fix template auction detail

--- app/assets/javascripts/public/channels/auction_detail.js
+++ app/assets/javascripts/public/channels/auction_detail.js
@@ -20,15 +20,7 @@ $(document).on('turbolinks:load', function () {
         received: function(data) {
           data = data.obj;
           $('#spanCountDown').html(time_convert(data.period));
-          $('#ContentPlaceHolder1_lbTitle').html(data.product_name);
           $('#product-price').html(formatMoney(data.product_price_start));
-          $('#detail-product').html(data.product_detail);
-          $('#first-image').html('<img src="' + data.product_pictures[0].file_name.url + '" alt="' + data.product_name + ' class="slow-images" style="max-width: 100%;border-bottom: 1px solid #ccc;border-right: 1px solid #ccc;"">');
-          html = "";
-          data.product_pictures.forEach(function (e) {
-             html += '<li class="product_thumb"><img class="slow-images" src="' + e.file_name.url + '"></li>';
-          });
-          $('#js-gallery').html(html);
         }
     });
   } else {
--- app/assets/javascripts/public/channels/auctions.js
+++ app/assets/javascripts/public/channels/auctions.js
@@ -15,9 +15,9 @@ App.auctions = App.cable.subscriptions.create('AuctionsChannel', {
     list.forEach(function (e) {
       html += '<div class="col-sm-6 col-md-3 ser">';
       html += '<div class="product-item thumbnail">';
-      html += '<a href="/auctions' + e.id + '"><img src="' + e.product_pictures[0].file_name.url + '" alt="' + e.product_name + '"></a>';
+      html += '<a href="/auctions/' + e.id + '"><img src="' + e.product_pictures[0].file_name.url + '" alt="' + e.product_name + '"></a>';
       html += '<div class="caption text-center">';
-      html += '<a href="/auctions'+ e.id +'"><h3>'+ e.product_name +'</h3></a>';
+      html += '<a href="/auctions/'+ e.id +'"><h3>'+ e.product_name +'</h3></a>';
       html += '<p><span id="countdowntimer" class="text-danger">'+ time_convert(e.period) + '</span></p>';
       html += '<span class= "last-bid-user rows"><img src="auctions.png" alt="' + e.product_name + '" class="icon-auction">' + formatMoney(e.product_price_start) + '</span>';
       html += '</div>'
