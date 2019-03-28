7c88f5282fea3656a2b30b883fce3b61ac09ecdc
at-luantran <luan.tran@asiantech.vn>
2019-03-28 11:54:41 +0700

    Improve add image

--- app/assets/javascripts/admin/main.js
+++ app/assets/javascripts/admin/main.js
@@ -161,3 +161,19 @@ $(document).ready(function () {
 //     });
 //   });
 });
+
+$(document).ready(function () {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#profile-img-tag').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#profile-img").change(function () {
+    readURL(this);
+  });
+});
--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,15 +1,14 @@
 module FormHelper
-  def form_group_for(form, field, opts = {}, &block)
+  def form_group_for(class_name, form, field, opts = {}, &block)
     label = opts.fetch(:label) { true }
     has_errors = form.object.errors[field].present?
 
-    content_tag :div, class: "form-group #{'has-error' if has_errors}" do
+    content_tag :div, class: "form-group #{class_name} #{'has-error' if has_errors}" do
       concat form.label(field, class: 'control-label') if label
       concat capture(&block)
       if has_errors
-        form.object.errors[field].each do |x|
-          concat content_tag(:p, x.to_s, class: 'help-block')
-        end
+        error = form.object.errors[field].first
+        concat content_tag(:span, error.to_s, class: 'help-block')
       end
     end
   end
